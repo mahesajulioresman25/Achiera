a4c6f0cd8a47ad006cfcb370453521a75ef7562f
reyhanfebrizioresman <reyhanfebrizioresman12@gmail.com>
2026-01-11 02:58:21 +0700

    chore: remove debug API routes and test pages
    - Remove src/app/test-route
    - Remove src/app/ping
    - Remove src/app/api/debug directory
    - Remove src/app/actions/rbac-demo.ts

--- src/app/ping/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PingPage() {
-    return (
-        <div>
-            <h1>Pong</h1>
-            <p>Deployment is working.</p>
-        </div>
-    );
-}
--- src/app/test-route/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-    return (
-        <div style={{ padding: 50, textAlign: 'center' }}>
-            <h1>Test Route Works</h1>
-            <p>System is routing correctly.</p>
-        </div>
-    );
-}
